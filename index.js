db6b54ac9678678b7dccff32cf90970c07d95264
praesidios <praesidios@gmail.com>
2019-08-24 23:35:18 +0300

    debug

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const cards = document.querySelectorAll('.hidden');
+const cards = document.querySelectorAll('.card');
 const button = document.querySelector('.next-button');
 
 
 button.addEventListener('click', function(){
   let visibleCard = document.querySelector('.show');
   let index = getRandomIndex();
+
   function getRandomIndex() {
     return Math.floor((Math.random() * 4));
   }
 
   for(let i = 0; i<=cards.length; i++){
     if(visibleCard){
-      visibleCard.className = "hidden";
+      visibleCard.className = "card";
     }
+    cards[index].className = "show";
   }
-  cards[index].className = "show";
+  
 });
+
