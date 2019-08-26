cf87e3afdc5883532516ac57f3aac698684de848
praesidios <praesidios@gmail.com>
2019-08-26 09:37:26 +0300

    finally

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const cards = document.querySelectorAll('.card');
-const button = document.querySelector('.next-button');
+const button = document.querySelector('.next');
+const resultCard = document.querySelector('.resultCard');
+const radio = document.querySelectorAll('.radio');
+const unique = [];
+let trueAnswer = 0;
+
+
+while(unique.length < cards.length){
+  let random = Math.floor((Math.random() * cards.length));
+  if(unique.indexOf(random) === -1) unique.push(random);
+}
 
 
 button.addEventListener('click', function(){
   let visibleCard = document.querySelector('.show');
-  let index = getRandomIndex();
+  let buttonText = document.querySelector('.next span');
+  let index = unique.pop();
 
-  function getRandomIndex() {
-    return Math.floor((Math.random() * 4));
+  buttonText.textContent = "Следующий вопрос";
+  button.style.display = "none";
+  button.style.padding = "10px";
+
+  if(cards[index] === undefined){
+    resultCardDesc(visibleCard);
   }
 
-  for(let i = 0; i<=cards.length; i++){
-    if(visibleCard){
-      visibleCard.className = "card";
-    }
-    cards[index].className = "show";
+  else if(visibleCard){
+    visibleCard.className = "card";
   }
-  
+  cards[index].className = "show";   
 });
 
+
+radio.forEach(function(checkedRadio){
+  checkedRadio.addEventListener('click', function(){
+    button.style.display = "block";
+    if(event.target.className === "radio true"){
+      trueAnswer++;
+    }
+  })
+})
+
+function resultCardDesc(visibleCard){
+  visibleCard.style.display = "none";
+  resultCard.style.display = "block";
+  button.style.display="none";
+
+  let resultWrap = document.createElement('h2');
+  resultCard.appendChild(resultWrap);
+  resultWrap.textContent = `${trueAnswer} / ${cards.length}`; 
+  resultWrap.style.textAlign = "center";
+  
+}
